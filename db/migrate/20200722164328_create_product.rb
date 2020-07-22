98a2d965d221eb21b714582b4aae93834b731250
Avinash Sarguru <avinashsarguru@gmail.com>
2020-07-22 14:14:05 -0500

    index skeleton

--- db/migrate/20200722164328_create_product.rb
+++ db/migrate/20200722164328_create_product.rb
@@ -1,8 +1,11 @@
 class CreateProduct < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.name :string
-      t.price :string
+      t.string :name
+      t.string :price
+      t.string :category
+      t.string :description
+      t.integer :rating
     end
   end
 end
